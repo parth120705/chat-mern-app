3619b04b59fbf7f77e154c110106d87d1016eada
parth120705 <munjalparth123@gmail.com>
2025-08-16 17:33:07 +0530

    Downgrade express to v4 for Render deploy

--- backend/src/index.js
+++ backend/src/index.js
@@ -27,13 +27,13 @@ app.use(cors({
 
 app.use("/api/auth",authRoutes)
 app.use("/api/messages",messageRoutes)
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
-      });
-}
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+  }
 
 server.listen(PORT,()=>{
     console.log("app is running at port 5001")
